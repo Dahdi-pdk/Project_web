3283e8db1e44e45bafea4e925229c6851c87e82a
Dahdi-pdk <dahdipdk@gmail.com>
2025-07-02 22:15:17 +0700

    pembuatan template awal

---
+++ script/script.js
@@ -0,0 +1,9 @@
+console.log("Script berhasil dimuat!");
+console.log("==========================================================");
+
+const toggleBtn = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
